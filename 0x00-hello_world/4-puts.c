e486382110f791c7f39da51a201ca4b62104306d
setty23 <settytek@gmail.com>
2023-03-16 17:58:28 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.>
+#include <stdio.h>
 /**
  * main - A program wit put function
- * Return: 0 
+ * Return: 0
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-}	
+}
