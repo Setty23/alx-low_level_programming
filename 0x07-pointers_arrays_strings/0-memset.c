163828b591d8e5ceec822f0e612d4a509b2e9ee1
setty23 <settytek@gmail.com>
2023-04-03 12:56:08 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int a = 0;
-	for (; n > 0; a++)
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
 	{
 		s[a] = b;
-		n--;
 	}
+
 	return (s);
 }
