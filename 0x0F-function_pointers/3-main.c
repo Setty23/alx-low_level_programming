2494f47286fa89a6889bbb51c0094c4d63fa6f28
setty23 <settytek@gmail.com>
2023-04-19 09:49:05 -0700

    *

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,16 +8,35 @@
  * @argv: args
  * Return: 0 on success
  */
-int main(int argc, char **argv)
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int result;
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	result = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
-	printf("%d\n", result);
+
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
+
