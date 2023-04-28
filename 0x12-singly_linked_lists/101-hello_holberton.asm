c961da12d0b97a15186f3dce3157af380cee0fa9
setty23 <settytek@gmail.com     git config --global user.email settytek@gmail.com>
2023-04-28 15:50:23 -0700

    *

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -4,8 +4,8 @@
 start:
 	  mov   edi, message
 	  xor   eax, eax
-	  mov   eax, 0
-          call  printf
-	  ret
+	  call  printf
+          mov   eax, 0
+          ret
 message: db "Hello, Holberton\n", 0
 
