2f0026c53221865823ec201c7affd5cbb597fa52
setty23 <settytek@gmail.com     git config --global user.email settytek@gmail.com>
2023-04-28 16:39:50 -0700

    *

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,9 @@
-	  global   start
-          extern   printf
-start:
+          global    main
+          extern    printf
+main:
 	  mov   edi, message
 	  xor   eax, eax
 	  call  printf
-          mov   eax, 0
-          ret
-
+	  mov 	eax, 0
+	  ret
 message: db "Hello, Holberton\n", 0
-
