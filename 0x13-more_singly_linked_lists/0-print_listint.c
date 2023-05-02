94139d0cba83bfd2f60a513816a623340eee25f5
setty23 <settytek@gmail.com     git config --global user.email settytek@gmail.com>
2023-05-02 13:16:20 -0700

    *

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include "lists.h"
 /**
-  * print_listint - prints all elements of a list_t list
+  * print_listint - prints all elements of a listnt_t list
   * @h: singly linked list to print
   * Return: number of nodes in the list
   */
 
 size_t print_listint(const listint_t *h)
 {
-	size_t i;
+	size_t i = 0;
 
-	for (i = 0; h; i++)
+	while (h != NULL)
 	{
-		printf("%d\n", h->n);
+		printf("%i\n", h->n);
 		h = h->next;
+		i++;
 	}
+
 	return (i);
 }
+
+	
