e958f44811793a0bcac89e015316b6c2be0d7d51
setty23 <settytek@gmail.com     git config --global user.email settytek@gmail.com>
2023-05-12 15:00:44 -0700

    *

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 	}
 	wr = write(op, text_content, len);
-	if (fd == -1 || wr == -1)
+	if (op == -1 || wr == -1)
 		return (-1);
 
 
