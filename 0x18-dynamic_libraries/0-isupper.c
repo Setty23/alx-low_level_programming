fe4cb8aed2487e5b570770d4a6ad2555d9dfdc71
setty23 <settytek@gmail.com>
2023-09-18 13:14:25 -0700

    update

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * _isupper - Check if a input is uppercase
+  * @c: The number to be checked
+  *
+  * Return: 1 for uppercase or 0 for otherwise
+  */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
+
