2f316953c50ab8a1a963c01c7bdc230ed3e3cacc
setty23 <settytek@gmail.com>
2023-09-18 13:44:10 -0700

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Setty23/alx-low_level_programming/raw/main/0x18-dynamic_libraries/giga.so
+wget -P /tmp https://github.com/setty23/alx-low_level_programming/raw/main/0x18-dynamic_libraries/giga.so
 export LD_PRELOAD=/tmp/giga.so
